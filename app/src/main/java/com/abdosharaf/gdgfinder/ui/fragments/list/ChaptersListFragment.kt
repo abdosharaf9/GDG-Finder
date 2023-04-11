17704ea88d14e7700207127763211e9dcaa669d2
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-04-11 04:01:07 +0200

    Change app icon & theme, and added filters to the list

--- app/src/main/java/com/abdosharaf/gdgfinder/ui/fragments/list/ChaptersListFragment.kt
+++ app/src/main/java/com/abdosharaf/gdgfinder/ui/fragments/list/ChaptersListFragment.kt
@@ -1,12 +1,16 @@
 package com.abdosharaf.gdgfinder.ui.fragments.list
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.abdosharaf.gdgfinder.R
 import com.abdosharaf.gdgfinder.databinding.FragmentChaptersListBinding
+import com.google.android.material.chip.Chip
 
 class ChaptersListFragment : Fragment() {
 
@@ -20,6 +24,31 @@ class ChaptersListFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.rvChapters.adapter = adapter
+
+        adapter.onItemClicked = { chapter ->
+            val destination = Uri.parse(chapter.website)
+            startActivity(Intent(Intent.ACTION_VIEW, destination))
+        }
+
+        viewModel.filters.observe(viewLifecycleOwner) { filters ->
+            val chipsGroup = binding.filters
+            val inflaterVar = LayoutInflater.from(chipsGroup.context)
+
+            val chips = filters.map { item ->
+                val chip = inflaterVar.inflate(R.layout.item_chip, chipsGroup, false) as Chip
+                chip.text = item
+                chip.tag = item
+                chip.setOnCheckedChangeListener { buttonView, isChecked ->
+                    viewModel.onFilterChanged(buttonView.tag as String, isChecked)
+                }
+                chip
+            }
+
+            chipsGroup.removeAllViews()
+            chips.forEach { chip ->
+                chipsGroup.addView(chip)
+            }
+        }
 
         return binding.root
     }
--- app/src/main/java/com/abdosharaf/gdgfinder/ui/fragments/list/ListViewModel.kt
+++ app/src/main/java/com/abdosharaf/gdgfinder/ui/fragments/list/ListViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.abdosharaf.gdgfinder.network.ChapterData
 import com.abdosharaf.gdgfinder.network.ChaptersResponse
 import com.abdosharaf.gdgfinder.network.apiService
 import kotlinx.coroutines.Dispatchers
@@ -12,8 +13,16 @@ import kotlinx.coroutines.launch
 class ListViewModel: ViewModel() {
 
     private val _chaptersResponse = MutableLiveData<ChaptersResponse>()
-    val chaptersResponse: LiveData<ChaptersResponse>
-        get() = _chaptersResponse
+
+    private val _chaptersList = MutableLiveData<List<ChapterData>?>()
+    val chaptersList: LiveData<List<ChapterData>?>
+        get() = _chaptersList
+
+    private val _filters = MutableLiveData<List<String>>()
+    val filters: LiveData<List<String>>
+        get() = _filters
+
+    private val filter = Filter()
 
     init {
         getChapters()
@@ -22,6 +31,38 @@ class ListViewModel: ViewModel() {
     private fun getChapters() {
         viewModelScope.launch {
             _chaptersResponse.value = apiService.getChapters()
+            _chaptersList.value = _chaptersResponse.value?.data
+            _filters.value = _chaptersResponse.value?.filters_?.region
+        }
+    }
+
+    fun onFilterChanged(newFilter: String, checked: Boolean) {
+        if(filter.updateFilter(newFilter, checked)) {
+            updateList(newFilter)
+        }
+    }
+
+    private fun updateList(filter: String) {
+        val list = _chaptersResponse.value?.data?.filter {
+            it.region == filter
+        }
+        _chaptersList.value = list
+    }
+
+    private inner class Filter {
+        var currentFilter: String? = null
+            private set
+
+        fun updateFilter(newFilter: String, newState: Boolean): Boolean {
+            return if(newState) {
+                currentFilter = newFilter
+                true
+            } else if(currentFilter == newFilter) {
+                currentFilter = null
+                true
+            } else {
+                false
+            }
         }
     }
 }
